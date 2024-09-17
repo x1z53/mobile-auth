41a2873d7ef71046176120ddead761294fb62c2a
Vladimir Vaskov <rirusha@altlinux.org>
2024-09-17 15:46:11 +0300

    Restruct

---
+++ mobileauth/welcome_dialog.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+# Authentication mobile - Alt Mobile domain input tool
+#
+# Copyright (C) 2024 Valentin Sokolov.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from gettext import gettext as _
+
+from gi.repository import Gtk, Adw, GLib
+
+
+@Gtk.Template(resource_path="/io/github/SokolovValy/MobileAuth/ui/welcome-dialog.ui")
+class WelcomeDialog(Adw.Dialog):
+
+    __gtype_name__ = "MobileAuthWelcomeDialog"
+
+    status_page:Adw.StatusPage = Gtk.Template.Child()
+
+    def __init__(self, domain_name):
+        super().__init__(title=_("Admin Data"))
+
+        status_page.props.title = _("Welcome to {}").format(domain_name)
